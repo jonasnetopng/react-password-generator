e200f889173447923cbb1ea94c5f797860c86339
Jonas <jonaseluna@gmail.com>
2022-01-28 03:45:11 -0300

    adding project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
+import './style.scss'
 
 function App() {
+  const [pass, setPass] = useState([])
+  const [copySuccess, setCopySuccess] = useState('');
+
+  var password = '';
+
+
+  function GeneratePassword(passwordLength) {
+    var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    for (var i = 0; i < passwordLength; i++) {
+      var rnum = Math.floor(Math.random() * chars.length);
+      password += chars.substring(rnum, rnum + 1);
+    }
+    const payload = {
+      passText: password,
+      data: new Date().toLocaleDateString()
+    }
+
+    return setPass(payload)
+  }
+
+  async function CopyPassword() {
+    try {
+      await navigator.clipboard.writeText(pass.passText);
+      setCopySuccess('Copiado!');
+    } catch (err) {
+      setCopySuccess('Erro ao copiar!');
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <div className='password_box'>
+          <h2>Gerar senha</h2>
+          <input placeholder={pass.passText === "" ? "Sua senha" : pass.passText} disabled />
+          <button className='button' onClick={() => GeneratePassword(16)}>Gerar</button>
+          <button className='button' onClick={() => CopyPassword()}>Copiar</button>
+        </div>
+        {Object.keys(pass).length > 1 && (
+          <div className="main">
+            <table>
+              <tr>
+                <th>Senha</th>
+                <th>Data</th>
+
+              </tr>
+              <tr>
+                <td>{pass.passText}</td>
+                <td>{pass.data}</td>
+              </tr>
+            </table>
+          </div>
+        )}
+      </div>
+    </>
+
   );
 }
 
