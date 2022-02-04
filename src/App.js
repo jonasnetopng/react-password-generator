fa6bafa4255b3efe9c1c69ac58566b3e1cb62dbf
Jonas <jonaseluna@gmail.com>
2022-02-04 14:01:46 -0300

    fix saved passwords

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import './style.scss'
+
+import { FiClipboard } from 'react-icons/fi'
 
 function App() {
   const [pass, setPass] = useState([])
   const [copySuccess, setCopySuccess] = useState('');
 
+  const [data, setData] = useState([]);
+
   var password = '';
+
+  async function getStorage() {
+    const passwords = await localStorage.getItem('password')
+
+    let passwordsSaves = JSON.parse(passwords) || [];
+
+    return passwordsSaves;
+  }
+
+  async function savePassoword(newLink) {
+    let passwords = await getStorage('password');
+
+    passwords.push(newLink);
+    await localStorage.setItem('password', JSON.stringify(passwords));
+  }
+  useEffect(() => {
+    async function getData() {
+      const passwords = await getStorage('password');
+      setData(passwords);
+    }
+    getData();
+  }, [pass]);
+
 
 
   function GeneratePassword(passwordLength) {
@@ -20,12 +47,12 @@ function App() {
       data: new Date().toLocaleDateString()
     }
 
-    return setPass(payload)
+    return (savePassoword(payload), setPass(payload));
   }
 
-  async function CopyPassword() {
+  async function CopyPassword(text) {
     try {
-      await navigator.clipboard.writeText(pass.passText);
+      await navigator.clipboard.writeText(text);
       setCopySuccess('Copiado!');
     } catch (err) {
       setCopySuccess('Erro ao copiar!');
@@ -40,20 +67,28 @@ function App() {
           <h2>Gerar senha</h2>
           <input placeholder={pass.passText === "" ? "Sua senha" : pass.passText} disabled />
           <button className='button' onClick={() => GeneratePassword(16)}>Gerar</button>
-          <button className='button' onClick={() => CopyPassword()}>Copiar</button>
+          <button className='button' onClick={() => CopyPassword(pass.passText)}>Copiar</button>
         </div>
-        {Object.keys(pass).length > 1 && (
+        {Object.keys(data).length > 0 && (
           <div className="main">
             <table>
               <tr>
                 <th>Senha</th>
                 <th>Data</th>
+                <th>Ações</th>
 
               </tr>
-              <tr>
-                <td>{pass.passText}</td>
-                <td>{pass.data}</td>
-              </tr>
+              {data.map((item, index) => (
+                <tr key={item.id}>
+                  <td>{item.passText}</td>
+                  <td>{item.data}</td>
+                  <td>
+                    <button onClick={() => CopyPassword(item.passText)}>
+                      <FiClipboard size={20} color="#000" />
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </table>
           </div>
         )}
